bcec868c7f65e29b93888384120d989f0c119722
Twin Tailed Fox <boyinthreeparts@gmail.com>
2026-09-14 17:42:22 +0100

    Basic NPC Behaviour Implemented
    Using Beehave to handle NPC Behaviour Trees. Works really well, should use this more for more complex enemy types.
    
    Next, work on the Battle Scene presentation, like attacks going back and forth and the like.

---
+++ Scripts/Character/Components/NPC Actions/npc_action.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name NPC_Action
+
+func execute(_delta: float) -> void:
+	pass
--- Scripts/Weapon/c_weapon.gd
+++ Scripts/Weapon/c_weapon.gd
@@ -33,7 +33,7 @@ func _ready() -> void:
 	end_recharge.connect(on_end_rechargee)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func on_charge_weapon():
