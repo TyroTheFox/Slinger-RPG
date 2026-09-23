6cc22520f2208e2361e8a36a4d7de7d9b4cb7dc1
Twin Tailed Fox <boyinthreeparts@gmail.com>
2026-09-23 19:47:54 +0100

    Added Enemy Animations

--- Scripts/Character/Components/NPC Actions/npc_a_basic_attack.gd
+++ Scripts/Character/Components/NPC Actions/npc_a_basic_attack.gd
@@ -2,6 +2,10 @@ extends NPC_Action
 class_name NPC_A_Basic_Attack
 
 @export var attack_power: int = 1
+@export var animation_player: AnimationPlayer
+@export var attack_animation_name: String = "attack"
 
 func execute(_delta: float) -> void:
+	animation_player.play(attack_animation_name)
+	await animation_player.animation_finished
 	get_tree().call_group("NPC_Component", "on_deal_damage", attack_power)
--- Scripts/Character/c_npc_character.gd
+++ Scripts/Character/c_npc_character.gd
@@ -3,8 +3,13 @@ class_name C_NPC_Character
 
 signal dead
 
-@onready var hp_component: C_HP_NPC = $C_HP
 @export var enemy_name = "Test"
+@export var beehave_tree: BeehaveTree
+@export var animation_player: AnimationPlayer
+@export var dead_animation_name: String = "dead"
+@export var take_damage_animation_name: String = "hurt"
+
+@onready var hp_component: C_HP_NPC = $C_HP
 @onready var enemy_stats: GUI_Enemy_Stat_Box = $enemy_stats
 
 # Called when the node enters the scene tree for the first time.
@@ -22,8 +27,18 @@ func on_deal_damage(damage_dealt: float):
 
 func on_take_damage(damage_taken: float):
 	hp_component.take_damage(damage_taken)
+	
+	if hp_component.alive:
+		animation_player.play(take_damage_animation_name)
+		await animation_player.animation_finished
+	
 	print("NPC TAKE DAMAGE: ", damage_taken)
 
 func on_dead():
+	beehave_tree.disable()
+	
+	animation_player.play(dead_animation_name)
+	await animation_player.animation_finished
+	
 	dead.emit()
 	print("NPC DEAD!")
